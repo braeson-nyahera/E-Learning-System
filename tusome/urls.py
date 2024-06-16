604077ee7f544cead0585b02e71da7d9e7ffab82
braeson_nyahera <braebulimo@gmail.com>
2024-06-16 17:03:59 +0300

    profile

--- tusome/urls.py
+++ tusome/urls.py
@@ -26,8 +26,9 @@ urlpatterns = [
     path('', include('home.urls')),
     path('courses', include('courses.urls')),
     path('register/',user_views.register,name='register'),
+    path('profile/',user_views.profile,name='profile'),
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
-    path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
+    path('logout/',user_views.logout_view,name='logout'),
 ]
 
 if settings.DEBUG:
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, ProfileUpdateForm, UserUpdateForm
+from django.contrib.auth import logout
 
 # Create your views here.
 def register(request):
@@ -17,3 +18,30 @@ def register(request):
         'form':form
     }
     return render(request, 'users/register.html',context)
+
+def profile(request):
+    if request.method=='POST':
+        u_form=UserUpdateForm(request.POST,instance=request.user)
+        p_form=ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+
+            messages.success(request,f'Your account has been updated.')
+            return redirect('profile')
+    
+    else:
+        u_form=UserUpdateForm(instance=request.user)
+        p_form=ProfileUpdateForm(instance=request.user.profile)
+
+    context={
+        'u_form':u_form,
+        'p_form':p_form
+    }
+
+    return render(request,'users/profile.html',context)
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
